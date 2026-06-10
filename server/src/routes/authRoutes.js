037bb41e299fe6bcf09888d31ebf45971f05f42c
Sanjay Chalawariya <sanjaychalawariya@Sanjays-MacBook-Air.local>
2026-06-11 00:33:14 +0530

    Change register route endpoint path to /signup

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { signup, login } = require('../controllers/authController');
 const router = express.Router();
 
-router.post('/register', signup);
+router.post('/signup', signup);
 router.post('/login', login);
 
 module.exports = router;
